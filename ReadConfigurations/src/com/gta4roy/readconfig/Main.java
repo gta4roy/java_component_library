31ed4bc9d85d393a252d6a961240ed08c41416d7
Abhijit <gta4roy@gmail.com>
2022-04-19 20:33:43 +0530

    Read Configuration Library

---
+++ ReadConfigurations/src/com/gta4roy/readconfig/Main.java
@@ -0,0 +1,9 @@
+package com.gta4roy.readconfig;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("This is basic library to read configurations from a file ");
+    }
+}
